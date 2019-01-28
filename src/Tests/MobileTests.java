4ad922400b2e7d2af762b411f532c000ba3bfbeb
Nico <nicolas.fazzano@gmail.com>
2019-01-28 16:24:12 -0300

    Casos Mobile 28/1

--- src/Tests/MobileTests.java
+++ src/Tests/MobileTests.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -18,6 +19,7 @@ import PageMetodos.Metodos;
 
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.util.List;
 import java.util.logging.Level;
 
 public class MobileTests extends Metodos {
@@ -71,8 +73,24 @@ public class MobileTests extends Metodos {
     		if (x.getText().contains("Larga Distancia Internacional Activado"))
     			ldi = true;
     	}
-    	driver.navigate().back();
     	return roaming && ldi;
+    }
+    
+    private boolean verificarLogin(String linea, String clave) {
+    	boolean password = false;
+    	sleep(5000);
+    	driver.findElement(By.id("custom_ab_drawer")).click();
+    	sleep(5000);
+    	driver.findElement(By.xpath("//*[@text='Cerrar Sesi\u00f3n']")).click();
+    	sleep(5000);
+    	driver.findElement(By.id("editTextLinea")).sendKeys(linea);
+    	driver.findElement(By.id("btn_log_in")).click();
+        driver.findElement(By.id("editTextPin")).sendKeys(clave);
+        driver.findElement(By.id("btn_log_in")).click();
+        if (driver.findElement(By.className("android.widget.TextView")).getText().contains("Los datos ingresados son incorrectos."))
+        	password = true;
+        scrollAndClick("xpath", "//android.widget.Button[@text='Aceptar']");
+        return password;
     }
        
   //------------------------------------------------------------------------------------------------------------------------------\\
@@ -98,6 +116,12 @@ public class MobileTests extends Metodos {
     
     //@AfterMethod (alwaysRun = true)
     public void after() {
+    	int menu = 0;
+    	while(((!driver.findElement(By.id("custom_ab_title")).getText().contains("Mi Personal"))) && menu < 3) {
+        	driver.navigate().back();
+        	menu++;
+        }	
+    	sleep(3000);
     	driver.findElement(By.id("custom_ab_drawer")).click();
     	sleep(5000);
     	driver.findElement(By.xpath("//*[@text='Cerrar Sesi\u00f3n']")).click();
@@ -204,11 +228,6 @@ public class MobileTests extends Metodos {
     	Assert.assertTrue(driver.findElement(By.id("button_component_text")).getText().contains("RECARG\u00c1 AHORA"));
     	driver.navigate().back();
     	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARGAS Y PACKS']");
-    	scrollAndClick("id", "button2");
-    	sleep(10000);
-    	Assert.assertTrue(driver.findElement(By.id("divRecargaPuntosClub")).isEnabled());
-    	driver.navigate().back();
-    	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARGAS Y PACKS']");
     	scrollAndClick("id", "button3");
     	sleep(10000);
     	Assert.assertTrue(driver.findElement(By.id("divRecargaTarjeta")).isEnabled());
@@ -247,14 +266,10 @@ public class MobileTests extends Metodos {
         sleep(5000);
         try {
         	Assert.assertTrue(driver.findElement(By.id("pdf_view")).isDisplayed());
-        } catch(Exception e) {
-        	for (int i=0; i<3; i++) {
-            	driver.navigate().back();
-        	}
+        	driver.navigate().back();
+        } catch(NoSuchElementException e) {
+        	driver.navigate().back();
         	Assert.assertTrue(false);
-        }
-        for (int i=0; i<3; i++) {
-            driver.navigate().back();
         }
     }
     
@@ -270,14 +285,10 @@ public class MobileTests extends Metodos {
         sleep(5000);
         try {
         	Assert.assertTrue(driver.findElement(By.id("pdf_view")).isDisplayed());
-        } catch(Exception e) {
-        	for (int i=0; i<3; i++) {
-            	driver.navigate().back();
-        	}
+        	driver.navigate().back();
+        } catch(NoSuchElementException e) {
+        	driver.navigate().back();
         	Assert.assertTrue(false);
-        }
-        for (int i=0; i<3; i++) {
-            driver.navigate().back();
         }
     }
     
@@ -307,10 +318,6 @@ public class MobileTests extends Metodos {
     	scrollAndClick("xpath", "//android.widget.TextView[@text='Cup\u00f3n de pago']");
     	driver.findElement(By.className("android.widget.EditText")).sendKeys("10.00");
     	//scrollAndClick("xpath", "//android.widget.TextView[@text='Descargar']");
-    	driver.navigate().back();
-    	sleep(5000);
-    	driver.navigate().back();
-    	sleep(5000);
     	Assert.assertTrue(false); //Mensaje de error al intentar descargar el cupon
     }
     
@@ -322,26 +329,126 @@ public class MobileTests extends Metodos {
     	scrollAndClick("xpath", "//android.widget.TextView[@text='Cup\u00f3n de pago']");
     	driver.findElement(By.className("android.widget.EditText")).sendKeys("10.00");
     	//scrollAndClick("xpath", "//android.widget.TextView[@text='Descargar']");
-    	driver.navigate().back();
-    	sleep(5000);
-    	driver.navigate().back();
-    	sleep(5000);
     	Assert.assertTrue(false); //Mensaje de error al intentar descargar el cupon
     }
     
     @Test (groups = "AutogestionIndividuosAPP")
     public void Recargas_Recarga_SOS_MIX() {
+    	boolean msj = false;
     	loginPorLineaMobile(driver, "MIX");
     	scrollAndClick("xpath", "//android.widget.TextView[@text='PAGOS, RECARGAS Y PACKS']");
     	driver.findElement(By.xpath("//*[@text='RECARGA S.O.S.']")).click();
     	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARG\u00c1 AHORA']");
+    	for (WebElement x : driver.findElements(By.className("android.widget.TextView"))) {
+    		if (x.getText().contains("La recarga S.O.S. se realiz\u00f3 con \u00e9xito"))
+    			msj = true;
+    	}
+    	scrollAndClick("xpath", "//android.widget.Button[@text='ACEPTAR']");
+    	Assert.assertTrue(msj);
     }
     
     @Test (groups = "AutogestionIndividuosAPP")
     public void Recargas_Recarga_SOS_PRE() {
+    	boolean msj = false;
     	loginPorLineaMobile(driver, "Pre");
     	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARGAS Y PACKS']");
     	driver.findElement(By.xpath("//*[@text='RECARGA S.O.S.']")).click();
     	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARG\u00c1 AHORA']");
+    	for (WebElement x : driver.findElements(By.className("android.widget.TextView"))) {
+    		if (x.getText().contains("La recarga S.O.S. se realiz\u00f3 con \u00e9xito"))
+    			msj = true;
+    	}
+    	scrollAndClick("xpath", "//android.widget.Button[@text='ACEPTAR']");
+    	Assert.assertTrue(msj);
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Recargas_Recarga_con_puntos_Club_MIX() {
+    	loginPorLineaMobileVariable(driver, "1168829219");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='PAGOS, RECARGAS Y PACKS']");
+    	driver.findElement(By.xpath("//*[@class='android.widget.RelativeLayout' and ./*[@text='Puntos Club']]")).click();
+    	scrollAndClick("xpath", "//android.view.View[@text='RECARGAS']");
+    	scrollAndClick("xpath", "//android.view.View[contains(text(),'Cr\u00e9dito $10')]");
+    	//scrollAndClick("xpath", "//android.view.View[@text='CANJEAR']");
+    	Assert.assertTrue(false);  //Mensaje de error al hacer click en canjear
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Recargas_Recarga_con_puntos_Club_PRE() {
+    	loginPorLineaMobileVariable(driver, "1164473518");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARGAS Y PACKS']");
+    	driver.findElement(By.xpath("//*[@class='android.widget.RelativeLayout' and ./*[@text='Puntos Club']]")).click();
+    	scrollAndClick("xpath", "//android.view.View[@text='RECARGAS']");
+    	scrollAndClick("xpath", "//android.view.View[contains(text(),'Cr\u00e9dito $10')]");
+    	//scrollAndClick("xpath", "//android.view.View[@text='CANJEAR']");
+    	Assert.assertTrue(false);  //Mensaje de error al hacer click en canjear
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Recargas_Gestiones_y_Consultas_Ultimas_Recargas_MIX() {
+    	int recargas = 0;
+    	loginPorLineaMobile(driver, "MIX");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='PAGOS, RECARGAS Y PACKS']");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='Historial de Recargas']");
+    	List<MobileElement> tabla = driver.findElement(By.xpath("//*[@class='android.widget.LinearLayout' and ./*[@id='complex_component_layout']]")).findElements(By.className("android.widget.TextView"));
+    	for (int i=0; i<tabla.size(); i++) {
+    		if (tabla.get(i).getText().contains("Recarga por Tarjeta de Credito"))
+    			recargas++;
+    	}
+    	Assert.assertTrue(recargas >= 1);
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Recargas_Gestiones_y_Consultas_Ultimas_Recargas_PRE() {
+    	int recargas = 0;
+    	loginPorLineaMobile(driver, "Pre");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='RECARGAS Y PACKS']");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='Historial de Recargas']");
+    	List<MobileElement> tabla = driver.findElement(By.xpath("//*[@class='android.widget.LinearLayout' and ./*[@id='complex_component_layout']]")).findElements(By.className("android.widget.TextView"));
+    	for (int i=0; i<tabla.size(); i++) {
+    		if (tabla.get(i).getText().contains("Recarga por Tarjeta de Credito"))
+    			recargas++;
+    	}
+    	Assert.assertTrue(recargas >= 1);
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Ayuda_Acceder_a_AYUDA_y_seleccionar_alguna_opcion() {
+    	boolean ayuda = false;
+    	loginPorLineaMobile(driver, "Pre");
+    	driver.swipe(315, 154, -12, 154, 84);
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='AYUDA']");
+    	driver.findElement(By.xpath("//*[@text='d. T\u00e9cnico | Personal Help - Argentina']")).click();
+    	sleep(7000);
+    	for (WebElement x : driver.findElements(By.className("android.view.View"))) {
+    		if (x.getText().contains("Mi equipo no reconoce la Sim"))
+    			ayuda = true;
+    	}
+    	Assert.assertTrue(ayuda);
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Login_Iniciar_Sesion_con_clave_Incorrecta() {
+    	loginPorLineaMobile(driver, "MIX");
+    	Assert.assertTrue(verificarLogin(lineaMIX, "1470"));
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Login_Iniciar_Sesion_sin_clave() {
+    	loginPorLineaMobile(driver, "MIX");
+    	Assert.assertTrue(verificarLogin(lineaMIX, ""));
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Login_Iniciar_Sesion_con_Linea_Inexistente() {
+    	loginPorLineaMobile(driver, "MIX");
+    	Assert.assertTrue(verificarLogin("1192735149", "1469"));
+    }
+    
+    @Test (groups = "AutogestionIndividuosAPP")
+    public void Login_Iniciar_Sesion_con_Linea_PreActiva() {
+    	loginPorLineaMobileVariable(driver, "1162645152");
+    	scrollAndClick("xpath", "//android.widget.TextView[@text='MIS SERVICIOS']");
+    	Assert.assertTrue(false);
     }
 }
