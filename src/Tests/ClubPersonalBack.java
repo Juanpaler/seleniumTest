1628be0dcde0990ffc67aaf5bb8257bf46721db1
Unknown <alejandro.mattioli@atos.net>
2019-03-25 11:30:15 -0300

    Casos club back

--- src/Tests/ClubPersonalBack.java
+++ src/Tests/ClubPersonalBack.java
@@ -25,7 +25,7 @@ public class ClubPersonalBack extends Metodos{
 		driver = setup();	
 	}
 	
-	@AfterMethod (alwaysRun = true)
+	//@AfterMethod (alwaysRun = true)
 	public void after(){
 		tomarCaptura(driver,nombreCaso);
 		try {
@@ -38,12 +38,50 @@ public class ClubPersonalBack extends Metodos{
 	
 	
 	@Test (groups = "ClubPersonalBack", priority = 0)
-	public void test() throws IOException{
+	public void Adhesion_Usuario_Titular_MIX() throws IOException{
 		nombreCaso = new Object(){}.getClass().getEnclosingMethod().getName();
-		//String linea=retornaLinea(nombreCaso,archivoLineas);
+		String linea=retornaLinea(nombreCaso,archivoLineas);
 		loginClubBack();	
-		
+		AdhesionTitularClubBack(linea,"testtest867");
+    }
 	
-}
+	@Test (groups = "ClubPersonalBack", priority = 0)
+	public void Adhesion_Usuario_Titular_POS() throws IOException{
+		nombreCaso = new Object(){}.getClass().getEnclosingMethod().getName();
+		String linea=retornaLinea(nombreCaso,archivoLineas);
+		loginClubBack();	
+		AdhesionTitularClubBack(linea,"testtest868");
+    }
 	
+	@Test (groups = "ClubPersonalBack", priority = 0)
+	public void Adhesion_Usuario_Titular_PRE() throws IOException{
+		nombreCaso = new Object(){}.getClass().getEnclosingMethod().getName();
+		String linea=retornaLinea(nombreCaso,archivoLineas);
+		loginClubBack();	
+		AdhesionTitularClubBack(linea,"testtest687");
+    }
+	
+	@Test (groups = "ClubPersonalBack", priority = 0)
+	public void Busqueda_por_Linea_MIX() throws IOException{
+		nombreCaso = new Object(){}.getClass().getEnclosingMethod().getName();
+		String linea=retornaLinea(nombreCaso,archivoLineas);
+		loginClubBack();	
+        busquedaLinea(linea);
+    }
+	
+	@Test (groups = "ClubPersonalBack", priority = 0)
+	public void Busqueda_por_Linea_POS() throws IOException{
+		nombreCaso = new Object(){}.getClass().getEnclosingMethod().getName();
+		String linea=retornaLinea(nombreCaso,archivoLineas);
+		loginClubBack();	
+        busquedaLinea(linea);
+    }
+	
+	@Test (groups = "ClubPersonalBack", priority = 0)
+	public void Busqueda_por_Linea_PRE() throws IOException{
+		nombreCaso = new Object(){}.getClass().getEnclosingMethod().getName();
+		String linea=retornaLinea(nombreCaso,archivoLineas);
+		loginClubBack();	
+        busquedaLinea(linea);
+    }
 }
