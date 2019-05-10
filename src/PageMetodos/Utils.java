c5d68b1ea6bd181e30a03c86263aec628353b0da
Ale <alejandro.mattioli@atos.net>
2019-05-10 16:28:31 -0300

    Caso C02_CuentaValidarDatosDelCliente

--- src/PageMetodos/Utils.java
+++ src/PageMetodos/Utils.java
@@ -301,5 +301,15 @@ public class Utils {
 	    	 return false;  
 	     }  
 	}
+	
+	public boolean doubleValue(String value) {  
+	     try{  
+			 Double.parseDouble(value);  
+			 return true;  
+	      } 
+	     catch (NumberFormatException e){  
+	    	 return false;  
+	     }  
+	}
   
 }
